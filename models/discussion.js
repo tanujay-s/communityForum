ead48c3b132d23b32ac44d4095e75f7d76d40f48
Tanujay Singh <122903126+tanujay-s@users.noreply.github.com>
2024-07-22 10:14:40 +0530

    Update discussion.js

--- models/discussion.js
+++ models/discussion.js
@@ -17,28 +17,3 @@ const Discussion = mongoose.model('Discussion', discussionSchema);
 module.exports = Discussion;
 
 
-
-// const mongoose = require('mongoose');
-
-// const discussionSchema = new mongoose.Schema({
-//     title: {
-//         type: String,
-//         required: true
-//     },
-//     content: {
-//         type: String,
-//         required: true
-//     },
-//     createdBy: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'User' // Reference to the User model for the user who created the discussion
-//     },
-//     createdAt: {
-//         type: Date,
-//         default: Date.now
-//     }
-// });
-
-// const Discussion = mongoose.model('Discussion', discussionSchema);
-
-// module.exports = Discussion;
