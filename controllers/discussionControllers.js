bf4ac27a65792c40925cc687484ebbbd03f43769
Tanujay Singh <122903126+tanujay-s@users.noreply.github.com>
2024-07-22 10:15:32 +0530

    Update discussionControllers.js

--- controllers/discussionControllers.js
+++ controllers/discussionControllers.js
@@ -1,6 +1,6 @@
 const Discussion = require('../models/discussion');
 
-// Function to create a new discussion
+
 const createDiscussion = async (req, res) => {
     try {
         const { title, content } = req.body;
@@ -17,7 +17,7 @@ const createDiscussion = async (req, res) => {
     }
 };
 
-// Function to get all discussions
+
 const getAllDiscussions = async (req, res) => {
     try {
         const discussions = await Discussion.find();
@@ -27,14 +27,14 @@ const getAllDiscussions = async (req, res) => {
     }
 };
 
-// // Function to add a comment to a discussion
+
 const addComment = async (req, res) => {
     try {
-        // Get the logged-in user's email from the session
+     
         const author = req.session.user.email;
         const { discussionId, content } = req.body;
 
-        // Validate the required fields
+   
         if (!content) {
             return res.status(400).json({ message: 'Content is required' });
         }
@@ -55,7 +55,6 @@ const addComment = async (req, res) => {
 };
 
 
-// Function to get comments for a specific discussion
 const getComments = async (req, res) => {
     try {
         const discussionId = req.params.id;
@@ -80,32 +79,6 @@ module.exports = {
 
 
 
-
-// const Discussion = require('../models/discussion');
-
-// const createDiscussion = async (req, res) => {
-//   try {
-//     const discussion = new Discussion({
-//       title: req.body.title,
-//       content: req.body.content,
-//       image: req.body.image
-//     });
-
-//     await discussion.save();
-//     res.status(201).json({ discussion });
-//   } catch (error) {
-//     res.status(400).json({ message: error.message });
-//   }
-// };
-
-// const getAllDiscussions = async (req, res) => {
-//   try {
-//     const discussions = await Discussion.find();
-//     res.json({ discussions });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
 
 // module.exports = {
 //     createDiscussion,
